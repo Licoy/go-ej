4564353a738dd2fd7918d958410a24e4690d7594
望梅不止渴 <licoycn@gmail.com>
2020-10-22 17:04:25 +0800

    更新打包脚本

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-go build -o excelToJson main.go
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o excelToJson.exe main.go
+go build -o ./build/excelToJson main.go
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o ./build/excelToJson.exe main.go
